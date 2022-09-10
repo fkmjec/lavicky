31d60a10ff0dd492fb4b63270219118812fbc1c0
František Kmječ <frantisek.kmjec@gmail.com>
2022-09-10 12:24:38 +0200

    Move data, some more text

--- README.md
+++ README.md
@@ -5,6 +5,31 @@ na sobě mají USB porty, ze kterých si občané města mohou nabít telefon.
 
 Operátor ICT, který lavičky provozuje, mezi 5.9.2017 a 31.1.2018 sbíral data o využívání USB portů,
 konkrétně za každý den celkový počet minut nabíjení. Mým cílem je v rámci práce zjistit, zda
-počet minut nabíjení souvisí s teplotou daný den v Praze.
+počet minut nabíjení koreluje s teplotou daný den v Praze.
 
-Data jsem o nabíjení čerpal jednak z již zmíněného [datasetu Operátora ICT](https://opendata.praha.eu/dataset/lavicky-api-usb), konkrétně z CSV, které obsahuje celkový počet minut u laviček z Alšova nábřeží. Jako data o teplotě jsem použil [dataset z pražského Klementina](https://www.chmi.cz/historicka-data/pocasi/praha-klementinum). Data o teplotě byla ve formátu `.xlsx`, jeho převod na CSV jsem provedl pomocí nástroje `ssconvert`.
+Data o nabíjení jsem čerpal jednak z již zmíněného [datasetu Operátora ICT](https://opendata.praha.eu/dataset/lavicky-api-usb), konkrétně z CSV, které obsahuje celkový počet minut u laviček z Alšova nábřeží. Jako data o teplotě jsem použil [dataset z pražského Klementina](https://www.chmi.cz/historicka-data/pocasi/praha-klementinum). Data o teplotě byla ve formátu `.xlsx`, jeho převod na CSV jsem provedl pomocí nástroje `ssconvert`.
+
+## Příprava dat
+Oba datasety potřebují jen mírnou přípravu. CSV s lavičkami kupodivu obsahuje data už z období mezi 1.7.2017 a 5.9.2017, která jsou ale z velké části nulová.
+Spolehl jsem se tedy na údaj uváděný na webové stránce datasetu, kde je jako začátek sběru dat uváděno 5.9.2017. Podle tohoto jsem ve skriptu oba datasety ořízl.  
+
+## První náhled
+Nejdříve si vykreslíme jak teploty, tak minuty nabíjení vedle sebe. Od oka to vypadá, že sledují podobný trend. Dávalo by to smysl; když je větší zima,
+lidé chodí méně ven a méně se sluní na lavičkách.
+
+![Teplota a minuty nabíjení v čase vedle sebe](assets/charging_temp_side_by_side.png)
+
+## Lineární regrese
+Následně pomocí lineární regrese zjistíme, zda průměrná teplota daného dne a počet minut nabíjení korelují.
+Nulová hypotéza je taková, že oba datasety jsou nekorelované. Zvolíme hladinu významnosti 0.05. Následně provedeme lineární regresi, jejíž výsledek je vidět na obrázku.
+
+![Lineární regrese](assets/regression.png)
+
+Z lineární regrese dostáváme p-hodnotu rovnou přibližně 0.0008, což je pod naší hladinou významnosti. Nulovou hypotézu tedy můžeme zamítnout a 
+prohlásit, že teplota a minuty nabíjení spolu korelují.  
+
+## Dny v týdnu a nabíjení
+Chceme zjistit, jestli počet minut nabíjení za den nějak souvisí s tím, jestli je pracovní den, nebo víkend. Nulová hypotéza je, že střední hodnoty jsou
+stejné pro pracovní den i víkend. Pro to, abychom to ověřili, použijeme studentův T-test. Trochu naivně budeme předpokládat nezávislost obou souborů vzorků.
+Opět si stanovíme hladinu významnosti na 0.05.
+Pomocí funkce `scipy.stats.ttest_ind()` provedeme test a dostaneme p mírně nad naší kritickou hodnotu. Nemůžeme tedy nulovou hypotézu.
--- plotting.py
+++ plotting.py
@@ -2,8 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 import scipy
+import numpy as np
 
-START_DATE = pd.Timestamp("2017-07-01")
+START_DATE = pd.Timestamp("2017-09-05")
 END_DATE = pd.Timestamp("2018-01-31")
 
 
@@ -22,19 +23,15 @@ def add_datetime(dataframe):
     return resulting_dataframe
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Example plotter for the statistical task")
-    parser.add_argument("--temperature-csv", type=str, required=True, help="The CSV filename to read temperatures from")
-    parser.add_argument("--bench-csv", type=str, required=True, help="The CSV filename to read bench data from")
-    args = parser.parse_args()
+def crop_bench_data(benches):
+    cond = (START_DATE <= benches["Datum"]) & (benches["Datum"] <= END_DATE)
+    resulting_benches = benches[cond]
+    return resulting_benches
 
-    temperature = pd.read_csv(args.temperature_csv)
-    cropped_dates = add_datetime(temperature)
-    benches = pd.read_csv(args.bench_csv)
-    # ax = plt.gca()
 
-    benches["Datum"] = pd.to_datetime(benches["Datum"])
-    fig, axes = plt.subplots(1, 2)
+def draw_side_by_side_plots(benches, temperature):
+    _fig, axes = plt.subplots(1, 2)
+    # Draw the two plots next to each other 
     t_axis = axes[0]
     b_axis = axes[1]
 
@@ -45,8 +42,61 @@ if __name__ == "__main__":
     b_axis.set_ylabel("Počet minut nabíjení")
 
     benches.plot(x="Datum", y="Nabijeni", kind="line", ax=b_axis)
-    cropped_dates.plot(x="date", y="T-AVG", ax=t_axis, color="red")
-
-    
-
+    temperature.plot(x="date", y="T-AVG", ax=t_axis, color="red")
     plt.show()
+
+
+def visualize_regression_results(x, y, reg_results):
+    print("=== Regression results ===")
+    print(f"regression results: {reg_results}")
+    regressed_values = x.copy()
+    assert regressed_values.shape == x.shape
+    regressed_values = regressed_values * reg_results.slope + reg_results.intercept
+    _fig, axis = plt.subplots(1, 1)
+
+    axis.set_xlabel("Průměrná teplota ve stupních Celsia")
+    axis.set_ylabel("Počet minut nabíjení")
+    
+    axis.scatter(x, y)
+    axis.plot(x, regressed_values)
+    plt.show()
+
+
+def split_days_of_week(benches):
+    weekend = []
+    weekday = []
+    for date in benches["Datum"]:
+        weekend.append(date.isoweekday() > 5)
+        weekday.append(date.isoweekday() <= 5)
+    return benches["Nabijeni"][weekend], benches["Nabijeni"][weekday]
+
+
+# test whether the 
+def ttest(weekends, weekdays):
+    print("=== T-test results for weekdays and weekends ===")
+    print(scipy.stats.ttest_ind(weekends, weekdays))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Example plotter for the statistical task")
+    parser.add_argument("--temperature-csv", type=str, required=True, help="The CSV filename to read temperatures from")
+    parser.add_argument("--bench-csv", type=str, required=True, help="The CSV filename to read bench data from")
+    args = parser.parse_args()
+
+    temperature = pd.read_csv(args.temperature_csv)
+    temperature = add_datetime(temperature)
+    benches = pd.read_csv(args.bench_csv)
+
+    benches["Datum"] = pd.to_datetime(benches["Datum"])
+    benches = crop_bench_data(benches)
+    
+    # draw an overview
+    draw_side_by_side_plots(benches, temperature)
+
+    # try linear regression to test correlation
+    regression_results = scipy.stats.linregress(temperature["T-AVG"], y=benches["Nabijeni"])
+    # print the results and draw a plot
+    visualize_regression_results(temperature["T-AVG"], benches["Nabijeni"], regression_results)
+
+    weekends, weekdays = split_days_of_week(benches)
+    ttest(weekends, weekdays)
+
